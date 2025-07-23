9777bf4f6f402d03eadc180c37f9949aa6f6f30c
Benjamin Tejas <57376142+bentejas@users.noreply.github.com>
2025-07-23 16:12:06 -0400

    made phone number smaller on mobile

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -19,7 +19,7 @@ const NavigationBar: React.FC = () => {
           </a>
           <a
             href="tel:5196144897"
-            className="text-white hover:text-gray-400 font-bold tracking-widest"
+            className="text-white hover:text-gray-400 font-bold tracking-widest text-sm sm:text-md"
           >
             (519) 614-4897
           </a>
